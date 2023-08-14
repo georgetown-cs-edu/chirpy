08fdf1ba38d09e364b50a93d5820298075295eec
Micah Sherr <msherr@cs.georgetown.edu>
2023-08-14 19:50:19 +0000

    moving things around

---
+++ edu/georgetown/WebService.java
@@ -0,0 +1,35 @@
+package edu.georgetown;
+
+import java.io.IOException;
+import java.util.logging.ConsoleHandler;
+import java.util.logging.FileHandler;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.logging.SimpleFormatter;
+
+import edu.georgetown.bll.user.UserService;
+
+
+public class WebService {
+
+    public static void main(String[] args) {
+        Logger logger = Logger.getLogger("MyLogger");
+        try {
+            FileHandler fileHandler = new FileHandler("/tmp/log.txt");
+            logger.addHandler(fileHandler);
+            fileHandler.setFormatter(new SimpleFormatter());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        ConsoleHandler consoleHandler = new ConsoleHandler();
+        logger.addHandler(consoleHandler);
+        // logger.setUseParentHandlers(false); // Remove default handlers
+        logger.setLevel(Level.ALL); // Set desired log level (e.g., Level.INFO, Level.WARNING, etc.)
+
+        logger.info("Starting chirpy web service");
+
+        new UserService(logger);
+        
+    }
+
+}
